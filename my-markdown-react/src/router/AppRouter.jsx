295865753945475aaa9d6fc2af8caf9ecf985814
Blairi <axel.avi.fer@gmail.com>
2022-10-10 18:55:32 -0500

    app pages and router

---
+++ my-markdown-react/src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import { AuthRouter } from "../auth/router/AuthRouter"
+import { MyMarkdownRouter } from "../MyMarkdown/router/MyMarkdownRouter"
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+
+      <Route path="auth/*" element={ <AuthRouter /> }/>
+
+      <Route path="/*" element={ <MyMarkdownRouter /> }/>
+    </Routes>
+  )
+}
