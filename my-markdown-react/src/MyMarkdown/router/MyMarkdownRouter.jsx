558918afc385f192c8388e688946671c7c9206e6
Blairi <axel.avi.fer@gmail.com>
2022-10-12 10:54:51 -0500

    dashboard route

--- my-markdown-react/src/MyMarkdown/router/MyMarkdownRouter.jsx
+++ my-markdown-react/src/MyMarkdown/router/MyMarkdownRouter.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom"
-import { HomePage } from "../pages"
+import { Dashboard, HomePage } from "../pages"
 
 export const MyMarkdownRouter = () => {
   return (
     <Routes>
 
       <Route path="/" element={ <HomePage /> }/>
+      <Route path="/dashboard" element={ <Dashboard /> }/>
 
     </Routes>
   )
