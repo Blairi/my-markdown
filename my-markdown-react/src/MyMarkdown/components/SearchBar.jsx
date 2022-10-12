07de4cf59505074d9aa56a24b7e8d6552b8633fc
Blairi <axel.avi.fer@gmail.com>
2022-10-12 15:41:57 -0500

    responsive

--- my-markdown-react/src/MyMarkdown/components/SearchBar.jsx
+++ my-markdown-react/src/MyMarkdown/components/SearchBar.jsx
@@ -6,11 +6,11 @@ export const SearchBar = () => {
       
       <input
         type="search"
-        className="p-5 block w-11/12 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        className="p-5 block w-10/12 sm:w-11/12 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
         placeholder="Search markdown title..."
       />
 
-      <button className="w-1/12 text-xl flex items-center justify-center text-white bg-indigo-500 hover:bg-indigo-800 transition">
+      <button className="w-2/12 sm:w-1/12 text-xl flex items-center justify-center text-white bg-indigo-500 hover:bg-indigo-800 transition">
         <FaSearch />
       </button>
     </div>
