961a46737f5cc257dc16ff3b9230c330087418a8
Blairi <axel.avi.fer@gmail.com>
2022-10-14 21:02:54 -0500

    new route added

--- my-markdown-react/src/MyMarkdown/router/MyMarkdownRouter.jsx
+++ my-markdown-react/src/MyMarkdown/router/MyMarkdownRouter.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import { DashboardRouterProvider } from "./DashboardRouterProvider"
-import { HomePage } from "../pages"
+import { CreateMarkdown, HomePage } from "../pages"
 
 export const MyMarkdownRouter = () => {
   return (
     <Routes>
 
       <Route path="/" element={ <HomePage /> }/>
+
       <Route path="/dashboard/*" element={ <DashboardRouterProvider /> }/>
+
+      <Route path="/markdowns/create" element={ <CreateMarkdown /> }/>
 
     </Routes>
   )
