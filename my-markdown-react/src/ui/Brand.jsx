52dcbe375deffd8ac29d5317e3d341d7abbb9fdc
Blairi <axel.avi.fer@gmail.com>
2022-10-18 16:45:53 -0500

    link added

--- my-markdown-react/src/ui/Brand.jsx
+++ my-markdown-react/src/ui/Brand.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom"
 
 export const Brand = () => {
   return (
-    <h1 className='text-2xl'>My<span className='text-black font-bold'>Markdown</span></h1>
+    <Link to='/'>
+      <h1 className='text-2xl'>My<span className='text-black font-bold'>Markdown</span></h1>
+    </Link>
   )
 }
