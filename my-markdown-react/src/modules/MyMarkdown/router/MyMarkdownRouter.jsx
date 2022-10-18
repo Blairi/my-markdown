a9372c69c44738d88e38e5f53677eca4d1a0644e
Blairi <axel.avi.fer@gmail.com>
2022-10-17 19:20:25 -0500

    app routes added

--- my-markdown-react/src/modules/MyMarkdown/router/MyMarkdownRouter.jsx
+++ my-markdown-react/src/modules/MyMarkdown/router/MyMarkdownRouter.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom"
-import { HomePage } from "../pages"
+import { Explore, HomePage, ShowMarkdown, UserMarkdowns } from "../pages"
 
 export const MyMarkdownRouter = () => {
   return (
     <Routes>
 
       <Route path="/" element={ <HomePage /> }/>
+
+      <Route path="/explore" element={ <Explore /> } />
+      
+      <Route path="/markdowns/1" element={ <ShowMarkdown /> } />
+
+      <Route path="/markdowns/user/1" element={ <UserMarkdowns /> } />
+
 
     </Routes>
   )
